e8c380d735d7f05d36aa3376f1b1f0913fffd909
psp <phulambrikarp@gmail.com>
2020-02-02 20:02:23 +0530

    Add project files.

---
+++ Core_WebApp/CustomFilters/MyfirstCustomFilter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySqlX.XDevAPI.Common;
+
+namespace Core_WebApp.CustomFilters
+{
+    public class MyfirstCustomFilter: ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            //You may fetch data from database here 
+            //filterContext.Controller.ViewBag.GreetMesssage = "Hello Foo";
+            //base.OnResultExecuting(filterContext);
+            string ControllerDetails;
+            ControllerDetails = filterContext.RouteData.Values["controller"].ToString();
+            viewData["action"] = context.RouteData.Values["action"];
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var controllerName = filterContext.RouteData.Values["controller"];
+            var actionName = filterContext.RouteData.Values["action"];
+            var message = String.Format("{0} controller:{1} action:{2}", "onactionexecuting", controllerName, actionName);
+            Debug.WriteLine(message, "Action Filter Log");
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
